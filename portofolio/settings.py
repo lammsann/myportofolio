82ee110bb8a272a3c8c1f9f18dd0bade72c41877
lammsann <ihsanghulam44@gmail.com>
2026-09-02 05:30:31 +0700

    Fix Allowed Host

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -15,7 +15,7 @@ import os
 from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "https://ghulam-muhammad-myportofolio.pws.cs.ui.ac.id/", "ghulam-muhammad-myportofolio.pws.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ghulam-muhammad-myportofolio.pws.cs.ui.ac.id"]
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
